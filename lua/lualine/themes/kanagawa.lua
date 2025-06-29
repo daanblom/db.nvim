25d5cc6d0204f37912f4c032b2cc8024fe28025a
daan <contact@daanblom.com>
2025-06-29 11:27:39 +0200

    replaced kanagawa with db everywhere

--- lua/lualine/themes/kanagawa.lua
+++ lua/lualine/themes/kanagawa.lua
@@ -1,43 +1,43 @@
-local theme = require("kanagawa.colors").setup().theme
+local theme = require("db.colors").setup().theme
 
-local kanagawa = {}
+local db = {}
 
-kanagawa.normal = {
+db.normal = {
   a = { bg = theme.syn.fun, fg = theme.ui.bg_m3 },
   b = { bg = theme.diff.change, fg = theme.syn.fun },
   c = { bg = theme.ui.bg_p1, fg = theme.ui.fg },
 }
 
-kanagawa.insert = {
+db.insert = {
   a = { bg = theme.diag.ok, fg = theme.ui.bg },
   b = { bg = theme.ui.bg, fg = theme.diag.ok },
 }
 
-kanagawa.command = {
+db.command = {
   a = { bg = theme.syn.operator, fg = theme.ui.bg },
   b = { bg = theme.ui.bg, fg = theme.syn.operator },
 }
 
-kanagawa.visual = {
+db.visual = {
   a = { bg = theme.syn.keyword, fg = theme.ui.bg },
   b = { bg = theme.ui.bg, fg = theme.syn.keyword },
 }
 
-kanagawa.replace = {
+db.replace = {
   a = { bg = theme.syn.constant, fg = theme.ui.bg },
   b = { bg = theme.ui.bg, fg = theme.syn.constant },
 }
 
-kanagawa.inactive = {
+db.inactive = {
   a = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
   b = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim, gui = "bold" },
   c = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
 }
 
-if vim.g.kanagawa_lualine_bold then
-  for _, mode in pairs(kanagawa) do
+if vim.g.db_lualine_bold then
+  for _, mode in pairs(db) do
     mode.a.gui = "bold"
   end
 end
 
-return kanagawa
+return db
