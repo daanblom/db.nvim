6a237c312b6623ea9ed3d0f1c4d2d586fff6e843
daan <contact@daanblom.com>
2025-07-03 23:47:34 +0200

    Color test

--- lua/db/colors.lua
+++ lua/db/colors.lua
@@ -5,9 +5,9 @@ local palette = {
     sumiInk0 = "#000000",
     sumiInk1 = "#000000",
     sumiInk2 = "#000000",
-    sumiInk3 = "#000000",
-    sumiInk4 = "#000000",
-    sumiInk5 = "#000000",
+    sumiInk3 = "#00FF00",
+    sumiInk4 = "#0000FF",
+    sumiInk5 = "#00FF00",
     sumiInk6 = "#333333", --fg
 
     -- Popup and Floats
