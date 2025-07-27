b112454869bf6475e23e4c7b21c055db814c3384
daan <contact@daanblom.com>
2025-07-27 15:06:34 +0200

    fixed fg colors dim

--- lua/db/themes.lua
+++ lua/db/themes.lua
@@ -198,26 +198,26 @@ return {
     dim = function(palette)
         return {
             ui = {
-                fg         = palette.dragonWhite,
+                fg         = palette.fujiWhite,
                 fg_dim     = palette.oldWhite,
                 fg_reverse = palette.waveBlue1,
 
-                bg_dim     = palette.dragonBlack1,
-                bg_gutter  = palette.dragonBlack4,
+                bg_dim     = palette.sumiInk1,
+                bg_gutter  = palette.sumiInk4,
 
-                bg_m3      = palette.dragonBlack0,
-                bg_m2      = palette.dragonBlack1,
-                bg_m1      = palette.dragonBlack2,
-                bg         = palette.dragonBlack3,
-                bg_p1      = palette.dragonBlack4,
-                bg_p2      = palette.dragonBlack5,
+                bg_m3      = palette.sumiInk0,
+                bg_m2      = palette.sumiInk1,
+                bg_m1      = palette.sumiInk2,
+                bg         = palette.sumiInk3,
+                bg_p1      = palette.sumiInk4,
+                bg_p2      = palette.sumiInk5,
 
-                special    = palette.dragonGray3,
-                whitespace = palette.dragonBlack6,
-                nontext    = palette.dragonBlack6,
+                special    = palette.springViolet1,
+                nontext    = palette.sumiInk6,
+                whitespace = palette.sumiInk6,
 
-                bg_visual  = palette.waveBlue1,
                 bg_search  = palette.waveBlue2,
+                bg_visual  = palette.sumiInk7,
 
                 pmenu      = {
                     fg       = palette.fujiWhite,
